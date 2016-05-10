8092f99b5cd779eeb02bf0037875bb2b6bd31184
Michael Azerhad <m.azerhad@wealcome.com>
2016-05-10 11:39:46 +0200

    adding some inputs

--- src/main/scala/com/sasu/incomecalculator/Main.scala
+++ src/main/scala/com/sasu/incomecalculator/Main.scala
@@ -4,8 +4,10 @@ object Main {
 
   def main(args: Array[String]) {
     val multipleFreelanceCriteria = List(
+      FreelanceCriteria(priceByDay = 350, nbYearlyWorkedDays = 212, fees = 8000, monthlySalary = 3000),
+      FreelanceCriteria(priceByDay = 400, nbYearlyWorkedDays = 212, fees = 8000, monthlySalary = 3000),
       FreelanceCriteria(priceByDay = 530, nbYearlyWorkedDays = 212, fees = 8000, monthlySalary = 3000),
-      FreelanceCriteria(priceByDay = 550, nbYearlyWorkedDays = 212, fees = 8000, monthlySalary = 3000)
+      FreelanceCriteria(priceByDay = 550, nbYearlyWorkedDays = 212, fees = 8000, monthlySalary = 2500)
     )
     val incomes = new SASUIncomeCalculator(new DefaultDueAmountRulesBeforeIncomeTax(new DefaultTaxSocietyAmount),
       new DefaultIncomeTaxRules(new DefaultTaxIncomeAmount)).calculate(multipleFreelanceCriteria)
